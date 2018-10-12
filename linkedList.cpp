58b63dcf9f3de6fb99085b33d8644fa424a24cde
Lukas <lukas.scharmitzer@gmail.com>
2018-10-12 15:57:22 +0200

    code reworked, comments added

--- linkedList.cpp
+++ linkedList.cpp
@@ -27,7 +27,7 @@ class List {
 		Node* getTail(){
 			return tail;
 		}
-		
+		//CREATING NODES
 		void createNode(int data) {
 
 			Node* temp = new Node;
@@ -42,14 +42,7 @@ class List {
 				tail = tail->next;
 			}
 		}
-		
-		void deleteNode(Node* node) {
-			Node* temp = new Node;
-			temp = head;
-			head = temp->next;
-			delete temp;
-		}
-		
+
 		void createFirstNode(int data) {
 			
 			Node* temp = new Node;	
@@ -62,45 +55,6 @@ class List {
 				temp->next = NULL;
 				head = temp;
 				tail = temp; 
-			}
-		}
-		
-		void deleteFromPosition(int data) {
-	
-			Node* prev = new Node;
-			prev = head;
-			
-			if(prev == NULL){
-				cout << "List is empty!" << endl;
-				return;
-			}else {
-				
-				Node* current = new Node;
-				current = head->next;
-				while(current->next != NULL){
-					if(prev->data == data){ //if previus node is right one (delete him)
-						head = current;
-						delete prev;
-						return;
-					}else if(current->data == data){
-						prev->next = current->next;
-						delete current;
-						cout << "Succesfully deleted " << data << endl; 
-						return;
-					}
-					prev = prev->next;
-					current = current->next;
-				}
-				
-				if(current->next == NULL && current->data == data) { 
-				
-					prev->next = current->next;
-					delete current;
-					cout << "Succesfully deleted " << data << endl; 
-					return;
-				}else {
-					cout << "Number is not in the list!" << endl;
-				}
 			}
 		}
 		
@@ -125,29 +79,88 @@ class List {
 			}
 		}
 		
-		void createNodeAfter(int limit, int data) {
+		void createAfter(int data, int flag){
 			
 			Node* temp = new Node;
-			temp->data = data;
-			Node* counter = new Node;
-			counter = head;
-			if(counter == NULL){
+			temp = head;
+			
+			while(temp != NULL){
+				if(temp->data == flag){
+					Node* x = new Node;
+					x->data = data;
+					x->next = temp->next;
+					temp->next = x;
+					cout << "Succesfully added" << endl;
+					return;
+				}
 				
+				temp = temp->next;
+			}
+		}
+			
+
+			
+
+		//DELETE NODES
+		void deleteNode(Node* node) {
+			Node* temp = new Node;
+			temp = head;
+			head = temp->next;
+			delete temp;
+		}
+		
+		void deleteLastNode() {
+			
+			Node* temp = new Node;
+			temp = head->next;
+			Node* x = head;
+			while(temp != NULL){
+				if(temp->next == NULL){
+					x->next = temp->next;
+					delete temp;
+					cout << "Succesfully deleted" << endl;
+					return;
+				}
+				temp = temp->next;
+				x = x->next;
+				
+			}
+		}
+		//delete node with data you choose
+		void deleteNodeWithData(int data) {
+	
+			Node* prev = new Node;
+			prev = head;
+			
+			if(prev == NULL){
 				cout << "List is empty!" << endl;
+				return;
+			}else if(prev->data == data){
+				head = prev->next;
+				delete prev;
+				cout << "Succesfully deleted number " << data << endl;
+				return;
+				
 			}else {
 				
-				while(counter->data != limit && counter->next != NULL){
-					counter = counter->next;
-					
-					if(counter->data == limit) {
-						
-						temp->next = counter->next;
-						counter->next = temp;		
+				Node* current = new Node;
+				current = head->next;
+				while(current->next != NULL){
+					current = current->next;
+					prev = prev->next;
+					if(current->data == data){
+						prev->next = current->next;
+						delete current;
+						cout << "Succesfully deleted number " << data << endl;
+						return;
 					}
 				}
 			}
-		}
-		
+			
+			cout << "Number " << data << " is not in the list." << endl;
+
+		}		
+
 		static void displayRecursion(Node* head) {
 				if(head == NULL){
 					cout << "NULL" << endl;
@@ -169,23 +182,51 @@ class List {
 				cout << "Both nodes are null!"; 
 			}
 		}
+		
+		static int listLenght(Node* head) {
+			Node* temp = new Node;
+			temp = head;
+			int counter = 0;
+			
+			while(temp != NULL){
+				counter ++ ;
+				temp = temp->next;
+			}
+			
+			return counter;
+		}
+		
+		static int nodeOnPosition(Node* head, int position){
+			Node* temp = new Node;
+			temp = head;
+			int counter = 0;
+			
+			while(temp != NULL){
+				if(counter == position){
+					return counter;
+				}
+				counter ++;
+				temp = temp->next;
+			}
+			
+			return 0;
+		}	
 };
 
 int main() {
 	
 	List a;
 	a.listInit();
-	
-	a.createNode(5);
-	a.createNode(10);
-	a.createNode(11);
-	a.createNode(12);
-	a.createNode(13);
-	a.createNode(14);
-	a.createNode(15);
-	a.createNode(16);					
+	a.createNode(1);
+	a.createNode(2);
+	a.createNode(3);
+	a.createNode(4);
+	a.createNode(5);			
 
-	List::displayRecursion(a.getHead());
+	cout << a.nodeOnPosition(a.getHead(), 11);
+
+	//List::displayRecursion(a.getHead());
+	//List::nThNode(13);
 	
 	return 0;
 }

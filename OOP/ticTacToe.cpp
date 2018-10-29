7d17995c05e2017c7282481c06bcfa9ac9a1885f
Lukas <lukas.scharmitzer@gmail.com>
2018-10-29 09:42:12 +0100

    clean layout

--- OOP/ticTacToe.cpp
+++ OOP/ticTacToe.cpp
@@ -27,80 +27,7 @@ public:
 	void printBoard();
 	bool checkPlay();
 };
-Game::Game() { 
-	this->empty = 0; 
-	play = true; 
-	createBoard();
-}
-void Game::createBoard() {
-	for (int i = 0; i < boardRows; i++) {
-		for (int j = 0; j < boardColumns; j++) {
-			board[i][j] = empty;
-		} 
-	}
-	currentPlayer = 1; 
-}
 
-void Game::move(int i, int j) {
-
-	board[i][j] = currentPlayer;
-	printBoard(); 
-
-	if (!isWinner(currentPlayer)) {
-		currentPlayer = -currentPlayer; 
-		return; 
-	}
-	cout << "winner is player: " << currentPlayer << endl;
-	return;
-}
-
-bool Game::isWinner(int mark) {
-	int winner = 3 * mark; 
-
-	if (((board[0][0] + board[0][1] + board[0][2]) == winner)		//1. row
-		|| ((board[1][0] + board[1][1] + board[1][2]) == winner)	//2. row
-		|| ((board[2][0] + board[2][1] + board[2][2]) == winner)	//3. row
-		|| ((board[0][0] + board[1][0] + board[2][0]) == winner)	//1. column
-		|| ((board[0][1] + board[2][1] + board[2][1]) == winner)	//2. column
-		|| ((board[0][2] + board[1][2] + board[2][2]) == winner)	//3. column
-		|| ((board[0][0] + board[1][1] + board[2][2]) == winner)	//1. diagonal
-		|| ((board[0][2] + board[1][1] + board[2][0]) == winner))	//2. diagonal
-	{
-		play = false; 
-		return true;
-	}
-	
-	return false; 
-}
-
-void Game::printBoard() {
-	for (int i = 0; i < boardRows; i++) {
-		for (int j = 0; j < boardColumns; j++) {
-			switch (board[i][j]) {
-			case O:
-				cout << "O";
-				break;
-			case X:
-				cout << "X";
-				break;
-			case 0:
-				cout << "";
-				break;
-			}
-			if (j < 2) 
-				cout << " | ";
-		}
-		if(i < 2)
-			cout << "\n-+--+-\n";
-	}
-}
-
-bool Game::checkPlay() {
-	if (!play)
-		return false;
-	
-	return true; 
-}
 int main()
 {
 	int i, j;
@@ -118,4 +45,77 @@ int main()
 
 	return 0;
 }
+Game::Game() {
+	this->empty = 0;
+	play = true;
+	createBoard();
+}
+void Game::createBoard() {
+	for (int i = 0; i < boardRows; i++) {
+		for (int j = 0; j < boardColumns; j++) {
+			board[i][j] = empty;
+		}
+	}
+	currentPlayer = 1;
+}
 
+void Game::move(int i, int j) {
+
+	board[i][j] = currentPlayer;
+	printBoard();
+
+	if (!isWinner(currentPlayer)) {
+		currentPlayer = -currentPlayer;
+		return;
+	}
+	cout << "winner is player: " << currentPlayer << endl;
+	return;
+}
+
+bool Game::isWinner(int mark) {
+	int winner = 3 * mark;
+
+	if (((board[0][0] + board[0][1] + board[0][2]) == winner)		//1. row
+		|| ((board[1][0] + board[1][1] + board[1][2]) == winner)	//2. row
+		|| ((board[2][0] + board[2][1] + board[2][2]) == winner)	//3. row
+		|| ((board[0][0] + board[1][0] + board[2][0]) == winner)	//1. column
+		|| ((board[0][1] + board[2][1] + board[2][1]) == winner)	//2. column
+		|| ((board[0][2] + board[1][2] + board[2][2]) == winner)	//3. column
+		|| ((board[0][0] + board[1][1] + board[2][2]) == winner)	//1. diagonal
+		|| ((board[0][2] + board[1][1] + board[2][0]) == winner))	//2. diagonal
+	{
+		play = false;
+		return true;
+	}
+
+	return false;
+}
+
+void Game::printBoard() {
+	for (int i = 0; i < boardRows; i++) {
+		for (int j = 0; j < boardColumns; j++) {
+			switch (board[i][j]) {
+			case O:
+				cout << "O";
+				break;
+			case X:
+				cout << "X";
+				break;
+			case 0:
+				cout << "";
+				break;
+			}
+			if (j < 2)
+				cout << " | ";
+		}
+		if (i < 2)
+			cout << "\n-+--+-\n";
+	}
+}
+
+bool Game::checkPlay() {
+	if (!play)
+		return false;
+
+	return true;
+}

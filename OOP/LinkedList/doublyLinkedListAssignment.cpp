9986fec6c7778a6a86ab9e0f038c89c312695903
Lukas <lukas.scharmitzer@gmail.com>
2018-11-12 15:45:24 +0100

    simplify functions

--- OOP/LinkedList/doublyLinkedListAssignment.cpp
+++ OOP/LinkedList/doublyLinkedListAssignment.cpp
@@ -1,69 +1,10 @@
-#include "pch.h"
-#include <iostream>
-#include <string>
-#include <vector>
-
-typedef std::string error;
-class Exeption {
-	error txt;
-public:
-	Exeption(error err)
-		:txt(err) {};
-	error getError() const {
-		return this->txt;
-	}
-};
-
-typedef std::string user;
-class Node {
-	user nickname;
-	double points;
-	Node* next;
-	friend class Scores;
-};
-
-class Scores {
-	Node* head;
-	int limit;
-	int counter;
-public:
-	Scores()
-		:head(NULL), limit(10), counter(1) {};
-	~Scores();
-	void add(const user, const double points);
-	void print();
-protected:
-	Node* lastNode();
-	void addFront(Node* newNode);
-	void addAfter(Node* newNode);
-	void removeBack();
-	bool isFull() const;
-	bool isEmpty() const;
-	void removeFront();
-};
-void menu(Scores& list);
-int main()
-{
-
-	Scores list;
-	menu(list);
-
-
-
-
-
-	return 0;
-}
 Scores::~Scores() {	//destrutor
 	while (!isEmpty()) {
 		removeFront();
 	}
 }
 bool Scores::isFull() const {
-	if (counter != limit) {
-		return false;
-	}
-	return true;
+	return (counter == limit); 
 }
 void Scores::addAfter(Node* newNode) {
 	if (!isEmpty()) {
@@ -95,7 +36,7 @@ Node* Scores::lastNode() {	//return last node of the list
 		return temp;
 	}
 	std::cout << "List is empty! \n";
-	return head; 
+	return head;
 }
 void Scores::print() {
 	if (!isEmpty()) {
@@ -119,79 +60,3 @@ void Scores::removeFront() {
 }
 void Scores::removeBack() {
 	Node* temp = head;
-	Node* prev = NULL;
-	while (temp != NULL) {	//traverse untill last node
-		if (temp->next == NULL) {
-			prev->next = NULL;
-			std::cout << "removed " << temp->nickname << " : " << temp->points << "\n";
-			delete temp;
-			return;
-		}
-		prev = temp;
-		temp = temp->next;
-	}
-}
-
-void Scores::add(const user nickname, const double points) {
-	//create new Node
-	Node* newNode = new Node;
-	newNode->nickname = nickname;
-	newNode->points = points;
-	if (head != NULL) {	//check if list is not empty
-		if (isFull()) {	// if list is full
-			if (points < lastNode()->points) {	//if last node points is higher than new node points
-				std::cout << "You failed \n";
-				delete newNode;
-				return;
-			}
-			removeBack();
-			addAfter(newNode);
-			return;
-		}
-		counter++; //add counter one point
-		addAfter(newNode);
-		return;
-	}
-	return addFront(newNode);
-}
-
-bool Scores::isEmpty() const {
-	if (head != NULL) {
-		return false;
-	}
-	return true;
-}
-
-void menu(Scores& list) {
-	double points;
-	int choice;
-	std::string nickname;
-	while (1) {
-		std::cout << "\n------------------------------------------\n";
-		std::cout << "Highscore list \n";
-		std::cout << "------------------------------------------ \n";
-		std::cout << "1. Add highscore \n";
-		std::cout << "2. Print list \n";
-		std::cout << "0. Exit \n";
-		std::cout << "Enter option: ";
-		std::cin >> choice;
-		std::cout << "\n";
-		switch (choice) {
-		case 1:
-			std::cout << "Enter nickname: ";
-			std::cin >> nickname;
-			std::cout << "Enter points: ";
-			std::cin >> points;
-			list.add(nickname, points);
-			break;
-		case 2:
-			list.print();
-			break;
-		case 0:
-			exit(1);
-			break;
-		default:
-			std::cout << "Wrong option!";
-		}
-	}
-}

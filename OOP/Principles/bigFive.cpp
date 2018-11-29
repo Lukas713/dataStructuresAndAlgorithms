30143e01fda9250b976eaa4bdc0c01ab54c7366d
Lukas <lukas.scharmitzer@gmail.com>
2018-11-29 12:11:24 +0100

    binary tree big 3 added

--- OOP/Principles/bigFive.cpp
+++ OOP/Principles/bigFive.cpp
@@ -1,63 +1,36 @@
-// testing.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include "pch.h"
-#include <iostream>
-#include <string>
-#include <vector>
-
-
-
-
-
-class intDigit {
-
-public:
-	intDigit(int max = 0)	//constructor
-		:storedValue(new int[max]) {}; 
-
-	~intDigit() {	//Destructor
-		delete storedValue; 
-	}
-
-	intDigit(const intDigit& x) {	//copy constructor
-		storedValue = new int[*x.storedValue]; 
-	}
-
-	intDigit(intDigit&& x)	//move constructor
-		:storedValue(x.storedValue)
-	{
-		x.storedValue = nullptr; 
-	}
-
-	intDigit& operator=(const intDigit& x) {	//copy assignment
-		if (this != &x)
-			*storedValue = *x.storedValue; 
-			return *this; 
-	}
-
-	intDigit& operator=(intDigit&& x) {		//move assignment
-		std::swap(storedValue, x.storedValue);
-		return *this; 
-	}
-
-private:
-	int* storedValue; 
-};
-
-
-int main()
-{
-
-	
-
-	
-	
-	
-	
-
-
-
-
-}
-
+class IntCell
+2 {
+3 public:
+4 explicit IntCell( int initialValue = 0 )
+5 { storedValue = new int{ initialValue }; }
+67
+~IntCell( ) // Destructor
+8 { delete storedValue; }
+9
+10 IntCell( const IntCell & rhs ) // Copy constructor
+11 { storedValue = new int{ *rhs.storedValue }; }
+12
+13 IntCell( IntCell && rhs ) : storedValue{ rhs.storedValue } // Move constructor
+14 { rhs.storedValue = nullptr; }
+15
+16 IntCell & operator= ( const IntCell & rhs ) // Copy assignment
+17 {
+18 if( this != &rhs )
+19 *storedValue = *rhs.storedValue;
+20 return *this;
+21 }
+22
+23 IntCell & operator= ( IntCell && rhs ) // Move assignment
+24 {
+25 std::swap( storedValue, rhs.storedValue );
+26 return *this;
+27 }
+28
+29 int read( ) const
+30 { return *storedValue; }
+31 void write( int x )
+32 { *storedValue = x; }
+33
+34 private:
+35 int *storedValue;
+36 };
--- OOP/Tree/binaryTree.cpp
+++ OOP/Tree/binaryTree.cpp
@@ -24,8 +24,8 @@ protected:	//only BinaryTree and derived class can access Node
 		Node* parent;	//link to parent node
 		Node* left;		//link to left node
 		Node* right;	//lin to right node
-		Node()	//constructor
-			:value(), parent(NULL), left(NULL), right(NULL) {};
+		Node(T val=0, Node* prnt = NULL, Node* lft = NULL, Node* rgt = NULL)	//constructor
+			:value(val), parent(prnt), left(lft), right(rgt) {};
 	};
 
 public:
@@ -40,36 +40,45 @@ public:
 		bool isExternal() const;	//check if node is external
 		friend class BinaryTree<T>; //BinaryTree can acces private members
 	private:
-		Node* v; //pointer to node
+		Node* v; //pointer node
 	};
 	typedef std::list<Position> PositionList;	 //list of positions
 
 	BinaryTree();
-	int size() const;	//check number of nodes
-	bool isEmpty() const;	//check if tree is empty
+	BinaryTree(const BinaryTree& T);
+	BinaryTree& operator=(BinaryTree T);
+	~BinaryTree();
 	Position getRoot() const; //get the root
 	PositionList positions() const; //get list of nodes
 	void addRoot();		//add root to empty tree
 	void expandEternal(const Position& p);	//expand external Node
 	Position removeAboveExternal(const Position& p); //remove p and parent
 	int depth(Position p);
+	int height(Position p);
 	void preorderPrint(Position p);
 	void postorderPrint(Position p);
-	void inorderPrint(Position p); 
+	void inorderPrint(Position p);
+	void externalLeft(Position p, int& i);
+
 protected:
 	void preorder(Node* v, PositionList& pl) const;
+	int size() const;	//check number of nodes
+	bool isEmpty() const;	//check if tree is empty
+	Node* clone(Node*  p) const;
+	void emptyTree(Position p);
 
 private:
-	Node* root;		//root pointer to first node
+	Node* root;	//root pointer to first node
 	int n;	//number of nodes 
 };
+
 
 int main()
 {
 
-
 	BinaryTree<int> T;
 	T.addRoot();	//create's root node 
+
 	BinaryTree<int>::Position p = T.getRoot();
 	*p = 313;
 	T.expandEternal(p);
@@ -82,16 +91,35 @@ int main()
 	*(p.right().left()) = 3;
 	*(p.right().right()) = 7;
 
-	int x = T.depth(p.right().left());
+	int x = T.depth(p.right());
 	T.preorderPrint(p);
 	std::cout << " ";
 	T.postorderPrint(p);
 	std::cout << " ";
-	T.inorderPrint(p); 
+	T.inorderPrint(p);
+	p = T.getRoot();
+	x = 0; 
+	T.externalLeft(p, x); 
+	std::cout << "\n number of left external elements: "; 
+	std::cout << x; 
+	p = T.getRoot(); 
+	x = T.height(p); 
+	std::cout << "\n height: " << x; 
+	x = T.depth(p.right().left()); 
+	std::cout << "\n depth: " << x; 
 
-
-
-
+ 
+	BinaryTree<int> L = T;
+	std::cout << "\n"; 
+	L.display(); 
+	std::cout << "\n"; 
+	T.display(); 
+	p = L.getRoot(); 
+	*p = 1; 
+	std::cout << "\n"; 
+	std::cout << *T.getRoot(); 
+	std::cout << "\n";
+	std::cout << *L.getRoot();
 
 	return 0;
 }
@@ -99,30 +127,33 @@ int main()
 Position methods
 */
 template <typename T>
-BinaryTree<T>::Position::Position(Node* x) {
+BinaryTree<T>::Position::Position(Node* x) {	//construct position node
 	this->v = x;
 }
 
 template <typename T>
-T& BinaryTree<T>::Position::operator*() {
+T& BinaryTree<T>::Position::operator*() {	//access value
 	return v->value;
 }
 
 template <typename T>
-typename BinaryTree<T>::Position BinaryTree<T>::Position::left() const {
+typename BinaryTree<T>::Position BinaryTree<T>::Position::left() const {	//construct position on left link
 	return Position(v->left);
 }
 
 template <typename T>
-typename BinaryTree<T>::Position BinaryTree<T>::Position::right() const {
+typename BinaryTree<T>::Position BinaryTree<T>::Position::right() const { //construct position on right link
 	return Position(v->right);
 }
 
 template <typename T>
-typename BinaryTree<T>::Position BinaryTree<T>::Position::parent() const {
+typename BinaryTree<T>::Position BinaryTree<T>::Position::parent() const { //construct position on parent
 	return Position(v->parent);
 }
-
+/*
+no param
+check if node is external
+*/
 template <typename T>
 bool BinaryTree<T>::Position::isExternal() const {
 	return (v->left == NULL && v->right == NULL);
@@ -131,33 +162,104 @@ bool BinaryTree<T>::Position::isExternal() const {
 BinaryTree methods declarations
 */
 template <typename T>
-bool BinaryTree<T>::Position::isRoot() const {
-	return (v->parent == NULL);
-}
-template <typename T>
 BinaryTree<T>::BinaryTree()
 	:root(NULL), n(0) {};
 
+template <typename T>
+BinaryTree<T>::~BinaryTree() {	//destroy whole tree
+	emptyTree(getRoot());
+}
+template <typename T>
+BinaryTree<T>::BinaryTree(const BinaryTree& T)	//copy constrcutor
+	:root(NULL), n(0) {
+	root = clone(T.getRoot().v); 
+}
+template <typename T>
+BinaryTree<T>& BinaryTree<T>::operator=(BinaryTree T) {	//assignment operator
+	if (this != &T) {
+		if (root != NULL) emptyTree(root); 
+		root = clone(T.getRoot().v); 
+	}
+	return *this; 
+}
+/*
+1 param: position
+visit left side, then right side
+clone Tree
+*/
+template <typename T>
+typename BinaryTree<T>::Node* BinaryTree<T>::clone(Node* p) const{	//clone with postorder
+	if (p == NULL) {
+		return p;
+	}
+	return new Node(p->value, p->parent, clone(p->left), clone(p->right));
+}
+/*
+param position
+clean whole tree with recursive calls
+no return value
+*/
+template <typename T>
+void BinaryTree<T>::emptyTree(Position p) {
+	if (p.v != NULL) {
+		emptyTree(p.left());
+		emptyTree(p.right());
+		delete p.v;
+	}
+	root = NULL;
+}
+/*
+no param
+check if node is root
+return bool
+*/
+template <typename T>
+bool BinaryTree<T>::Position::isRoot() const {
+	return (v->parent == NULL);
+}
+/*
+no param
+number of nodes
+return int 
+*/
 template <typename T>
 int BinaryTree<T>::size() const {
 	return n;
 }
 
+/*
+no param
+checks if tree is empty
+return bool
+*/
 template <typename T>
 bool BinaryTree<T>::isEmpty() const {
 	return (n == 0);
 }
-
+/*
+no param
+construct Position on root
+return Position
+*/
 template <typename T>
 typename BinaryTree<T>::Position BinaryTree<T>::getRoot() const {
 	return Position(root);
 }
+/*
+no param
+add's root, set n to 1
+no ret value
+*/
 template <typename T>
 void BinaryTree<T>::addRoot() {
 	root = new Node;
 	n = 1;
 }
-
+/*
+1 param: const referance to position
+creates two siblings
+no ret value
+*/
 template <typename T>
 void BinaryTree<T>::expandEternal(const Position& p) {	//expand external node
 	Node* v = p.v;			//set pointer to external position
@@ -167,7 +269,11 @@ void BinaryTree<T>::expandEternal(const Position& p) {	//expand external node
 	v->right->parent = v;	//set right's parent
 	n += 2;			//increment number of nodes by two
 }
-
+/*
+1 param: const referance to Position
+delete Position Node and he's parent
+return new Position
+*/
 template <typename T>
 typename BinaryTree<T>::Position BinaryTree<T>::removeAboveExternal(const Position& p) {	//remove p and p's parent
 	Node* position = p.v;
@@ -192,7 +298,12 @@ typename BinaryTree<T>::Position BinaryTree<T>::removeAboveExternal(const Positi
 	n -= 2;		//reduce number of nodes
 	return Position(sibling);
 }
-
+/*
+2 params: pointer to Node, referance to List
+fill list with nodes
+reursive calls through whole list
+no return value
+*/
 template <typename T>
 void BinaryTree<T>::preorder(Node* v, PositionList& L) const {	//list of all node
 	L.push_back(Position(v)); //add this node
@@ -203,14 +314,22 @@ void BinaryTree<T>::preorder(Node* v, PositionList& L) const {	//list of all nod
 		preorder(v->right, L); //recursivve call for right call of the tree
 	}
 }
-
+/*
+no param
+creates list, fill it with nodes
+return PositionList
+*/
 template <typename T>
 typename BinaryTree<T>::PositionList BinaryTree<T>::positions() const {	//list of all node
 	PositionList L;
 	preorder(root, L);	//traverse the tree and stores nodes inside list
-	return PositionList(L);		//return resulting list
+	return PositionList(L);	//return resulting list
 }
-
+/*
+1 param: Position
+compute depth of Node
+return int
+*/
 template <typename T>
 int BinaryTree<T>::depth(Position p) {
 	if (p.isRoot()) {
@@ -248,3 +367,30 @@ void BinaryTree<T>::inorderPrint(Position p) {	//with inorder traversal, node is
 		inorderPrint(p.right());
 	}
 }
+/*
+Algorithm for counting the number of left external nodes in a
+binary tree, using the Binary tree ADT.
+*/
+template <typename T>
+void BinaryTree<T>::externalLeft(Position p, int& i) {
+	if (p.v == NULL) return;
+
+	if (p.isExternal() && p.parent().left().v == p.v) {
+		i++;
+	}
+	externalLeft(p.left(), i);
+	externalLeft(p.right(), i);
+}
+/*
+return height of tree
+argument position
+test if its NULL,
+*/
+template <typename T>
+int BinaryTree<T>::height(Position p) {
+	if (p.v == NULL) {
+		return -1;
+	}
+	return 1 + std::max(height(p.left()), height(p.right()));
+}
+

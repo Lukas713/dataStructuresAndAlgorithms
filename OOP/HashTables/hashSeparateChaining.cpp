ff1f88cac1bd0443745a3a04ef5e41bc882ec26f
Lukas <lukas.scharmitzer@gmail.com>
2018-12-11 17:28:40 +0100

    separate chaining Heap

--- OOP/HashTables/hashSeparateChaining.cpp
+++ OOP/HashTables/hashSeparateChaining.cpp
@@ -5,119 +5,203 @@
 #include <map>
 #include <vector>
 
-
-template <typename hashedOBJ>
-class HashTable {
-public:
-	HashTable(int size = 101);
-	bool contains(const hashedOBJ& me) const; 
-
-	void makeEmpty();
-	bool insert(const hashedOBJ& x); 
-	bool insert(hashedOBJ&& rvalue); 
-	bool remove(const hashedOBJ& x); 
-
-private:
-	std::vector<std::list<hashedOBJ>> list;		//array of lists
-	int currentSize; 
-	
-	void rehash(); 
-	size_t myhash(const hashedOBJ& o) const; 
+template <typename K, typename V>
+class Entry { // a (key, value) pair
+	K key; // key
+	V value; // value
+public: // public functions
+	Entry(const K& k = K(), const V& v = V()) // constructor
+		: key(k), value(v) { }
+	const K& getKey() const { return key; } // get key (read only)
+	const V& getValue() const { return value; } // get value (read only)
+	void setKey(const K& k) { key = k; } // set key
+	void setValue(const V& v) { value = v; } // set value
 };
 
+template <typename K, typename V, typename H>
+class HashMap {
+public: // public types
+	typedef Entry<const K, V> Entry; // a (key,value) pair
+	class Iterator; // a iterator/position
+public: // public functions
+	HashMap(int capacity = 100); // constructor
+	int size() const; // number of entries
+	bool empty() const; // is the map empty?
+	Iterator find(const K& k); // find entry with key k
+	Iterator put(const K& k, const V& v); // insert/replace (k,v)
+	void erase(const K& k); // remove entry with key k
+	void erase(const Iterator& p); // erase entry at p
+	Iterator begin(); // iterator to first entry
+	Iterator end(); // iterator to end entry
+protected: // protected types
+	typedef std::list<Entry> Bucket; // a bucket of entries
+	typedef std::vector<Bucket> BktArray; // a bucket array
+	Iterator finder(const K& k); // find utility
+	Iterator inserter(const Iterator& p, const Entry& e); // insert utility
+	void eraser(const Iterator& p); // remove utility
+	typedef typename BktArray::iterator BItor; // bucket iterator
+	typedef typename Bucket::iterator EItor; // entry iterator
+	static void nextEntry(Iterator& p) // bucket’s next entry
+	{
+		++p.ent;
+	}
+	static bool endOfBkt(const Iterator& p) // end of bucket?
+	{
+		return p.ent == p.bkt->end();
+	}
+private:
+	int n; // number of entries
+	H hash; // the hash comparator
+	BktArray B; // bucket array
+public: // public types
+	class Iterator { // an iterator (& position)
+	private:
+		EItor ent; // which entry
+		BItor bkt; // which bucket
+		const BktArray* ba; // which bucket array
+	public:
+		Iterator(const BktArray& a, const BItor& b, const EItor& q = EItor())
+			: ent(q), bkt(b), ba(&a) { }
+		Entry& operator*() const; // get entry
+		bool operator==(const Iterator& p) const; // are iterators equal?
+		Iterator& operator++(); // advance to next entry
+		friend class HashMap; // give HashMap access
+	};
+};
 
-
-/*
-polynomial hash fnction 
-
-size_t represents size of the object
-A class that implements a hash table algorithm can then use calls
-to the generic hash function object to generate an
-integral type size_t and then scale the result into a suitable array index. In hash tables,
-this is manifested in private member function myhash
-*/
-template <typename T>
-class hash {
+template <typename K>
+class Hash {
 public:
 	size_t operator()(const std::string& key) {
-		size_t hashVal = 0; 
-
-		for (char ch : key) 
-			hashVal = 37 * hashVal + ch; 
-		return hashVal; 
+		size_t hashValue = 0;
+		for (char ch : key)
+			hashValue = 37 * hashValue + ch;
+		return hashValue;
 	}
 };
 
-
-
-
 int main()
 {
-	
 
-	HashTable<std::string> a; 
-	a.insert("Lukas"); 
-	
+
+	HashMap <std::string, int, Hash<std::string>> A;
+	A.put("Lukas", 2);
+
+
+
+
+
 
 
 
 
 	return 0;
 }
-template <typename hashedOBJ>
-size_t HashTable<hashedOBJ>::myhash(const hashedOBJ& o) const {
-	hash<hashedOBJ> hf;
-	return hf(o) % list.size();	//construct hash and insert in list 
+template <typename K, typename V, typename H> // get entry
+typename HashMap<K, V, H>::Entry&
+HashMap<K, V, H>::Iterator::operator*() const
+{
+	return *ent;
 }
-template <typename hashedOBJ>
-void HashTable<hashedOBJ>::makeEmpty() {
-	for (auto& theList : list) {	//remove's all lists from buckets
-		list.clear();
-	}
-}
-template <typename hashedOBJ>
-bool HashTable<hashedOBJ>::contains(const hashedOBJ& me) const {
-	auto& whichList = list[myhash(me)];		//construct hash with argumented object, catch bucket
-	return std::find(std::begin(whichList), std::end(whichList), me) != end(whichList);
-}
-template <typename hashedOBJ>
-bool HashTable<hashedOBJ>::remove(const hashedOBJ& obj) {
-	auto& tempList = list[myhash(obj)];
-	auto& iterator = std::find(std::begin(tempList), std::end(tempList), obj);
 
-	if (iterator == std::end(tempList)) {
-		std::cout << "\nElement not found!\n";
-		return false;
-	}
-	tempList.erase(iterator);
-	currentSize--;
-	return true;
+template <typename K, typename V, typename H>
+bool HashMap<K, V, H>::Iterator::operator==(const Iterator& p) const {
+	if (ba != p.ba || bkt != p.bkt) return false; // ba or bkt differ?
+	else if (bkt == ba->end()) return true; // both at the end?
+	else return (ent == p.ent); // else use entry to decide
 }
-template <typename hashedOBJ>
-void HashTable<hashedOBJ>::rehash() {
-	std::vector<std::list<hashedOBJ>> oldList = list;
-	list.resize(2 * list.size());
 
-	for (auto & lists : list) {
-		list.clear();
+template <typename K, typename V, typename H>
+typename HashMap<K, V, H>::Iterator& HashMap<K, V, H>::Iterator::operator++() {
+	++ent; // next entry in bucket
+	if (endOfBkt(*this)) { // at end of bucket?
+		++bkt; // go to next bucket
+		while (bkt != ba->end() && bkt->empty()) // find nonempty bucket
+			++bkt;
+		if (bkt == ba->end()) return *this; // end of bucket array?
+		ent = bkt->begin(); // first nonempty entry
 	}
-	/*copy table over*/
-	currentSize = 0;
-	for (auto& thisList : oldList) {
-		for (auto& x : thisList) {
-			insert(std::move(x));
-		}
+	return *this; // return self
+}
+
+template <typename K, typename V, typename H>
+typename HashMap<K, V, H>::Iterator HashMap<K, V, H>::end()
+{
+	return Iterator(B, B.end());
+}
+
+template <typename K, typename V, typename H>
+typename HashMap<K, V, H>::Iterator HashMap<K, V, H>::begin() {
+	if (empty()) return end(); // emtpty - return end
+	BItor bkt = B.begin(); // else search for an entry
+	while (bkt->empty()) ++bkt; // find nonempty bucket
+	return Iterator(B, bkt, bkt->begin()); // return first of bucket
+}
+
+template <typename K, typename V, typename H>
+HashMap<K, V, H>::HashMap(int capacity) : n(0), B(capacity) { }
+
+template <typename K, typename V, typename H>
+int HashMap<K, V, H>::size() const { return n; }
+
+template <typename K, typename V, typename H>
+bool HashMap<K, V, H>::empty() const { return size() == 0; }
+
+template <typename K, typename V, typename H>
+typename HashMap<K, V, H>::Iterator HashMap<K, V, H>::finder(const K& k) {
+	int i = hash(k) % B.size(); // get hash index i
+	BItor bkt = B.begin() + i; // the ith bucket
+	Iterator p(B, bkt, bkt->begin()); // start of ith bucket
+	while (!endOfBkt(p) && (*p).getKey() != k) // search for k
+		nextEntry(p);
+	return p; // return final position
+}
+template <typename K, typename V, typename H>
+typename HashMap<K, V, H>::Iterator HashMap<K, V, H>::find(const K& k) {
+	Iterator p = finder(k); // look for k
+	if (endOfBkt(p)) // didn’t find it?
+		return end(); // return end iterator
+	else
+		return p; // return its position
+}
+
+template <typename K, typename V, typename H>
+typename HashMap<K, V, H>::Iterator HashMap<K, V, H>::inserter(const Iterator& p, const Entry& e) {
+	EItor ins = p.bkt->insert(p.ent, e); // insert before p
+	n++; // one more entry
+	return Iterator(B, p.bkt, ins); // return this position
+}
+
+template <typename K, typename V, typename H>
+typename HashMap<K, V, H>::Iterator HashMap<K, V, H>::put(const K& k, const V& v) {
+	Iterator p = finder(k); // search for k
+	if (endOfBkt(p)) { // k not found?
+		return inserter(p, Entry(k, v)); // insert at end of bucket
+	}
+	else { // found it?
+		p.ent->setValue(v); // replace value with v
+		return p; // return this position
 	}
 }
-template <typename hashedOBJ>
-bool HashTable<hashedOBJ>::insert(const hashedOBJ& x) {
-	auto& tempList = list[myhash(x)];
-	if (std::find(std::begin(tempList), std::end(tempList), x) != std::end(tempList))
-		return false;
-	list.push_back(x);
 
-	if (++currentSize > list.size()) {
-		rehash();
+template <typename K, typename V, typename H>
+void HashMap<K, V, H>::eraser(const Iterator& p) {
+	p.bkt->erase(p.ent); // remove entry from bucket
+	n--; // one fewer entry
+}
+
+template <typename K, typename V, typename H>
+void HashMap<K, V, H>::erase(const Iterator& p)
+{
+	eraser(p);
+}
+
+template <typename K, typename V, typename H>
+void HashMap<K, V, H>::erase(const K& k) {
+	Iterator p = finder(k); // find k
+	if (endOfBkt(p)) { // not found?
+		std::cout << "error";
+		return;
 	}
-	return true;
+	eraser(p); // remove it
 }

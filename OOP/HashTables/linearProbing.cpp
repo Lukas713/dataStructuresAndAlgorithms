52148de083de96f293abc5e7e97500a1812b7025
Lukas <lukas.scharmitzer@gmail.com>
2018-12-17 13:22:40 +0100

    added nextPrime() and prevPrime()

--- OOP/HashTables/linearProbing.cpp
+++ OOP/HashTables/linearProbing.cpp
@@ -10,19 +10,27 @@
 template <typename K>
 class Hash {
 public:
-	unsigned int operator()(const K& key) {
+	unsigned int operator()(std::string key) {
 		unsigned int hashValue = 0;
 		for (char ch : key)
 			hashValue = 37 * hashValue + ch;
 		return hashValue;
 	}
+
+	unsigned int operator()(unsigned int key) {
+		key = ((key >> 16) ^ key) * 0x45d9f3b;
+		key = ((key >> 16) ^ key) * 0x45d9f3b;
+		key = (key >> 16) ^ key;
+		return key;
+	}
+
 };
 
 template <typename K>
 class EqualKeys {
 public:
 	bool operator()(const K& one, const K& other) {
-		return (one == other); 
+		return (one == other);
 	}
 };
 
@@ -35,18 +43,17 @@ class HashMap {
 
 		Node(const K& k = K(), const V& v = V(), bool a = true)	//flag is true at construct as default
 			:key(k), value(v), available(a) {}
-		Node(const Node& x); 
+		Node(const Node& x);
 		void setValue(const V& v);
 		void setKey(const K& k);
 		K getKey();
 		V getValue();
 		bool isAvailable();	//checks flag
-		void changeStatus();	//change flag bool
 	};
-	typedef typename Node Node; 
-	typedef typename std::vector<Node> List; 
-	typedef typename List::iterator ListIterator; 
-	typedef typename List::const_iterator constListIterator; 
+	typedef typename Node Node;
+	typedef typename std::vector<Node> List;
+	typedef typename List::iterator ListIterator;
+	typedef typename List::const_iterator constListIterator;
 public:
 	class iterator {
 	public:
@@ -54,31 +61,31 @@ public:
 			:node(p), list(&l) {}
 		iterator(const iterator& p)
 			:node(p.node), list(p.list) {}
-		Node& operator*(); 
+		Node& operator*();
 		iterator& operator++();
-		iterator& operator++(int); 
+		iterator& operator++(int);
 		bool operator==(const iterator& p) const;
 		bool operator!=(const iterator& p) const;
-		friend class HashMap<K, V, H, E>; 
+		friend class HashMap<K, V, H, E>;
 	private:
-		ListIterator node; 
-		const List* list; 
+		ListIterator node;
+		const List* list;
 	};
 
 public:
 	/*public methods*/
 	HashMap(int capacity = 101)
 		: L(capacity), n(0) {}
-	int size() const; 
-	bool isEmpty() const; 
+	int size() const;
+	bool isEmpty() const;
 	iterator insert(const K& key, const V& value);
 	iterator find(const K& key);
-	void erase(const K& key); 
+	void erase(const K& key);
 	iterator begin();
 	iterator end();
 	void display() {	//print whole table
 		for (int i = 0; i < L.size(); i++) {
-			std::cout << i + 1 << ". " << L[i].getKey() << " : " << L[i].getValue() << " : " << L[i].available << " \n"; 
+			std::cout << i + 1 << ". " << L[i].getKey() << " : " << L[i].getValue() << " : " << L[i].available << " \n";
 		}
 	}
 private:
@@ -90,30 +97,36 @@ private:
 
 
 	/*HashMap properties*/
-	std::vector<Node> L; 
+	std::vector<Node> L;
 	int n;	//number of elements in the list
 	Hash<K> hash;
-	EqualKeys<K> testKeys; 
+	EqualKeys<K> testKeys;
 };
 
+/*find next prime number for resize()*/
+bool isPrime(int x);
+
+int nextPrime(int n);
+
+int prevPrime(int n); 
 
 int main()
 {
-	HashMap <std::string, int, Hash<std::string>, EqualKeys<std::string>> a; 
-	HashMap <std::string, int, Hash<std::string>, EqualKeys<std::string>>::iterator ca; 
-	a.insert("a", 3); 
-	a.insert("b", 4); 
-	a.insert("c", 5); 
-	a.insert("c", 7); 
-	a.erase("b"); 
-	a.erase("a"); 
-	a.insert("a", 5);
-	a.insert("a", 6); 
-	a.insert("jk", 81); 
-	a.insert("c", 991); 
-
 	
+	
+	HashMap <int, int, Hash<int>, EqualKeys<int>> a;
+
+	for (int i = 0; i < 200; i++) {
+		if (a.size() == 51) {
+			std::cout << "da"; 
+		}
+		a.insert(rand() % 1000, rand() % 100);
+
+	}
+
 	a.display(); 
+	std::cout << "\n" << a.size(); 
+
 
 	
 
@@ -121,7 +134,9 @@ int main()
 
 
 
+	
 
+	 
 
 	return 0;
 }
@@ -181,19 +196,19 @@ modify pre increment operator overload
 template <typename K, typename V, typename H, typename E>
 typename HashMap<K, V, H, E>::iterator& HashMap<K, V, H, E>::iterator::operator++() {
 	++node;
-	return *this; 
+	return *this;
 }
 
 /*
-no param 
+no param
 move iteratr to next node
 modify post increment operator overload
 */
 template <typename K, typename V, typename H, typename E>
 typename HashMap<K, V, H, E>::iterator& HashMap<K, V, H, E>::iterator::operator++(int) {
-	auto temp = *this; 
-	++(*this); 
-	return temp; 
+	auto temp = *this;
+	++(*this);
+	return temp;
 }
 
 /*
@@ -202,7 +217,7 @@ access operator overload
 access to node iterator property
 */
 template <typename K, typename V, typename H, typename E>
-typename HashMap<K, V, H, E>::Node& HashMap<K, V, H, E>::iterator::operator*(){
+typename HashMap<K, V, H, E>::Node& HashMap<K, V, H, E>::iterator::operator*() {
 	return *node;
 }
 
@@ -218,7 +233,7 @@ typename HashMap<K, V, H, E>::iterator HashMap<K, V, H, E>::begin() {
 
 /*
 no param
-construct Iterator with node pointer as property to element after last 
+construct Iterator with node pointer as property to element after last
 return Iterator object
 */
 template <typename K, typename V, typename H, typename E>
@@ -235,25 +250,25 @@ return iterator object
 template <typename K, typename V, typename H, typename E>
 typename HashMap<K, V, H, E>::iterator HashMap<K, V, H, E>::insert(const K& key, const V& value) {
 	if (n > (L.size() / 2)) {	//if half of vector is full
-		resize(L.size() * 2);
+		resize(nextPrime(L.size()));
 	}
-	
+
 	int i = hash(key) % L.size();
-	for ( i ; i < L.size(); ((i + 1) % size())) {
+	for (i; i < L.size(); ((i += 1) % L.size())) {
 		if (!L[i].isAvailable() && testKeys(L[i].getKey(), key)) { //node with key and value is already inserted
-			iterator p(L, L.begin() + i); 
+			iterator p(L, L.begin() + i);
 			(*p).setValue(value);
 			return p;
 		}
 		if (L[i].isAvailable()) {	//find spot that is available
-			Node temp = Node(key, value, false); 
-			L[i] = temp; 
+			Node temp = Node(key, value, false);
+			L[i] = temp;
 			iterator p(L, L.begin() + i);
-			n++; 
+			n++;
 			return p;
 		}
-	} 
-	return end(); 
+	}
+	return end();
 }
 /*
 1 param: int
@@ -263,14 +278,7 @@ no return value
 */
 template <typename K, typename V, typename H, typename E>
 void HashMap<K, V, H, E>::resize(int capacity) {
-	HashMap* newHashMap = new HashMap(capacity);
-
-	for (int i = 0; i < n; i++) {
-		if (L[i].isAvailable())
-			continue;
-		newHashMap->insert(L[i].getKey(), L[i].getValue());
-	}
-	newHashMap->n = n;
+	L.resize(capacity); 
 }
 /*
 1 param: K (const referance)
@@ -336,7 +344,7 @@ void HashMap<K, V, H, E>::eraser(iterator& position) {
 	}
 	n--;
 	if (n > 0 && n <= (L.size() / 8)) {
-		resize(L.size() / 2);
+		resize(prevPrime(L.size()));
 	}
 }
 
@@ -345,7 +353,7 @@ Node methods declaration
 */
 template <typename K, typename V, typename H, typename E>
 void HashMap<K, V, H, E>::Node::setValue(const V& v) {
-	this->value = v; 
+	this->value = v;
 }
 template <typename K, typename V, typename H, typename E>
 void HashMap<K, V, H, E>::Node::setKey(const K& k) {
@@ -353,7 +361,7 @@ void HashMap<K, V, H, E>::Node::setKey(const K& k) {
 }
 template <typename K, typename V, typename H, typename E>
 V HashMap<K, V, H, E>::Node::getValue() {
-	return value; 
+	return value;
 }
 template <typename K, typename V, typename H, typename E>
 K HashMap<K, V, H, E>::Node::getKey() {
@@ -363,12 +371,31 @@ template <typename K, typename V, typename H, typename E>
 bool HashMap<K, V, H, E>::Node::isAvailable() {
 	return available;
 }
-template <typename K, typename V, typename H, typename E>
-void HashMap<K, V, H, E>::Node::changeStatus() {
-	available = !available;
-}
 
 template <typename K, typename V, typename H, typename E>
 HashMap<K, V, H, E>::Node::Node(const Node& x)
 	: value(x.value), key(x.key), available(x.available) {}
 
+/*find next prime number*/
+bool isPrime(int x) {
+	for (int i = 2; i < x; i++) {
+		if (x % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int nextPrime(int n) {
+	while (!isPrime(++n));
+	return n;
+}
+
+int prevPrime(int n) {
+	int x = n; 
+	while (!isPrime(--x));
+	if (x < 3) {
+		return n; 
+	}
+	return x;
+}

7fec817f03f7168a7753e8d3883f3fc195c9e4ce
Lukas <lukas.scharmitzer@gmail.com>
2019-01-08 19:28:36 +0100

    removed unessecery brackets

--- OOP/Algorithms/selectionSort.cpp
+++ OOP/Algorithms/selectionSort.cpp
@@ -5,25 +5,26 @@
 #include <map>
 #include <vector>
 
-
-
 void selectionSort(std::vector<int>& V, int size) {
 	int min;
 	for (int i = 0; i < size; i++) {
 		for (int j = i + 1; j < size; j++) {
-			if (V[i] > V[j]) {
+			if (V[i] > V[j]) 
 				std::swap(V[i], V[j]); 
-			}
+			
 		}
 	}
 }
+
+
 int main()
 {
 	std::vector<int> A = { 9, 1, 8, 2, 7, 3, 6, 4, 5 }; 
 	selectionSort(A, 9); 
-	for (int i = 0; i < 9; i++) {
+	
+	for (int i = 0; i < 9; i++) 
 		std::cout << A[i] << " "; 
-	}
+	
 	
 
 	return 0;

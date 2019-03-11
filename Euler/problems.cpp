a3db364d3a4870bcf5d26c7a0285c3bfbb5db7a5
lukas <lukas.scharmitzer@gmail.com>
2019-03-11 21:49:38 +0100

    added few more problems

--- Euler/problems.cpp
+++ Euler/problems.cpp
@@ -2,31 +2,58 @@
 #include <cmath>
 #include <vector>
 
-using namespace std;
+/**
+ *  problems from https://projecteuler.net/archives
+ */
 
-
+/**
+ * 1)
+ */
 int sumfOfThreesOrFivesBelow(int x);
 
+/**
+ * 2)
+ */
 int sumOfEvenInsideFibonacciBelow(int x);
 
+/**
+ * 3)
+ */
 long long largestPrimeFactorOf(long long x);
-bool isPrime(long long x);
+bool isPrimeFactor(long long x);
 
+/**
+ * 4)
+ */
 bool isPalindrome(int x);
 int findLargestPalindromeFrom(int digits);
 
+/**
+ * 6)
+ */
+int differenceBetweenSumedSquares();
+
+/**
+ * 7)
+ */
+bool isPrime(int x);
+
+int tenTousandAndFirstPrime();
+
+
 int main() {
 
+    int x = tenTousandAndFirstPrime();
+    //std::cout << x ;
 
-    int x = findLargestPalindromeFrom(3);
-    std::cout << x;
+
 
 
 
     return 0 ;
 }
 /**
- * Find the sum of all the multiples of 3 or 5 below 1000.
+ * 1) Find the sum of all the multiples of 3 or 5 below 1000.
  */
 int sumfOfThreesOrFivesBelow(int n) {
     int sum = 0;
@@ -40,7 +67,7 @@ int sumfOfThreesOrFivesBelow(int n) {
 }
 
 /**
- * Find the sum of the even-valued terms inside Fibonacci sequence whose values do not exceed four million.
+ * 2) Find the sum of the even-valued terms inside Fibonacci sequence whose values do not exceed four million.
  */
 int sumOfEvenInsideFibonacciBelow(int n) {
     int x = 1, y = 0, z = 0, sum = 0;
@@ -59,12 +86,12 @@ int sumOfEvenInsideFibonacciBelow(int n) {
 }
 
 /**
- * What is the largest prime factor of the number 600851475143
+ * 3) What is the largest prime factor of the number 600851475143
  */
 long long largestPrimeFactorOf(long long x){
     int largestPrimeFactor = 0;
     for(long long i = 3; i < sqrt(x); i+= 2){
-        if(x % i == 0 && isPrime(i)){
+        if(x % i == 0 && isPrimeFactor(i)){
             std::cout << i << " ---------------prime na factor \n";
             largestPrimeFactor = i;
         }
@@ -72,7 +99,7 @@ long long largestPrimeFactorOf(long long x){
     return largestPrimeFactor;
 }
 
-bool isPrime(long long x){
+bool isPrimeFactor(long long x){
     for(int i=3; i<=sqrt(x); i+=2){
         if(x % i == 0)
             return false;
@@ -81,7 +108,7 @@ bool isPrime(long long x){
 }
 
 /**
- * Find the largest palindrome made from the product of two 3-digit numbers.
+ * 4) Find the largest palindrome made from the product of two 3-digit numbers.
  */
 bool isPalindrome(int x){
     int n = x, i = 1, z = 0, j = 1;
@@ -123,4 +150,44 @@ int findLargestPalindromeFrom(int digits){
         --i;
     }
     return palindrome;
+}
+
+/**
+ * 6) Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ */
+int differenceBetweenSumedSquares() {
+    int j = 0, z = 0;
+
+    for(int i=1; i<=100; ++i){
+        z += pow(i, 2);
+        j += i;
+    }
+
+    return pow(j, 2) - z;
+}
+
+/**
+ * 7) What is the 10 001st prime number?
+ */
+int tenTousandAndFirstPrime() {
+    int i = 1;
+    int j = 2;
+
+    while(i <= 10001){
+        if(isPrime(j)){
+            std::cout << i << ": " << j << "\n";
+            ++i;
+        }
+        ++j;
+    }
+    return j;
+}
+
+bool isPrime(int x){
+    for(int i=2; i <= x / 2; ++i){
+        if(x % i != 0)
+            continue;
+        return false;
+    }
+    return true;
 }
